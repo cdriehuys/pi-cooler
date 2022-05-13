f8bc22ed0b2f2551af1313f740e31742ceb347b8
Chathan Driehuys <chathan@driehuys.com>
2022-05-13 22:11:38 +0000

    feat: Open metrics server.
    The application reports the CPU temperature in the Open Metrics format
    on port 8000.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,108 @@
-use std::{error::Error, fs, thread, time::Duration};
+#[macro_use]
+extern crate lazy_static;
 
-use rppal::pwm::Pwm;
+use std::{
+    convert::Infallible,
+    error::Error,
+    fs,
+    net::SocketAddr,
+    sync::{atomic::AtomicU64, Arc},
+    time::Duration,
+};
+
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+use prometheus_client::{
+    encoding::text::encode,
+    metrics::{family::Family, gauge::Gauge},
+    registry::Registry,
+};
 
 const CPU_TEMP_FILE: &str = "/sys/class/thermal/thermal_zone0/temp";
 
-fn main() -> Result<(), Box<dyn Error>> {
-    match read_cpu_temp(CPU_TEMP_FILE) {
-        Ok(temp) => {
-            println!("CPU Temp: {:.1} C", temp);
+lazy_static! {
+    static ref CPU_TEMP: Family<String, Gauge<f64, AtomicU64>> = Family::default();
+}
+
+async fn encode_metrics(registry: Arc<Registry>) -> Result<Response<Body>, Infallible> {
+    let mut encoded_metrics = vec![];
+    encode(&mut encoded_metrics, &registry).unwrap();
+
+    Ok(Response::new(encoded_metrics.into()))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut registry = <Registry>::default();
+    registry.register(
+        "cpu_temp",
+        "Current CPU temperature in Celsius",
+        Box::new(CPU_TEMP.clone()),
+    );
+
+    let cpu_temp_task = tokio::task::spawn(async { poll_cpu_temp(CPU_TEMP_FILE).await });
+
+    let mut encoded_metrics = vec![];
+    encode(&mut encoded_metrics, &registry).unwrap();
+
+    println!("{}", String::from_utf8(encoded_metrics)?);
+
+    let addr: SocketAddr = "0.0.0.0:8000".parse()?;
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let registry_ref = Arc::new(registry);
+    let make_svc = make_service_fn(move |_conn| {
+        let registry_ref = registry_ref.clone();
+        async move {
+            // service_fn converts our function into a `Service`
+            Ok::<_, Infallible>(service_fn(move |_req: Request<Body>| {
+                let registry_ref = registry_ref.clone();
+                encode_metrics(registry_ref)
+            }))
         }
-        Err(err) => {
-            eprintln!("Failed to read CPU temperature: {:?}", err);
-        }
-    };
+    });
 
-    let pin = Pwm::with_frequency(
-        rppal::pwm::Channel::Pwm0,
-        50.0,
-        0.25,
-        rppal::pwm::Polarity::Normal,
-        true,
-    )?;
+    let server = Server::bind(&addr).serve(make_svc);
 
-    thread::sleep(Duration::from_secs(3));
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 
-    pin.set_duty_cycle(0.5)?;
-    thread::sleep(Duration::from_secs(3));
-
-    pin.set_duty_cycle(0.75)?;
-    thread::sleep(Duration::from_secs(3));
-
-    pin.set_duty_cycle(1.0)?;
-    thread::sleep(Duration::from_secs(3));
+    cpu_temp_task.await?;
 
     Ok(())
 }
 
-fn read_cpu_temp(path: &str) -> Result<f32, Box<dyn Error>> {
-    let raw_temp = fs::read_to_string(path)?;
-    let parsed_temp_millis: f32 = raw_temp.trim().parse()?;
+#[derive(Clone)]
+struct AppState {
+    registry: Arc<Registry>,
+}
 
-    Ok(parsed_temp_millis / 1000_f32)
+fn read_cpu_temp(path: &str) -> Result<f64, Box<dyn Error>> {
+    let raw_temp = fs::read_to_string(path)?;
+    let parsed_temp_millis: f64 = raw_temp.trim().parse()?;
+
+    Ok(parsed_temp_millis / 1000_f64)
+}
+
+async fn poll_cpu_temp(path: &str) {
+    let mut interval = tokio::time::interval(Duration::from_secs(1));
+
+    loop {
+        interval.tick().await;
+
+        match read_cpu_temp(path) {
+            Ok(temp) => {
+                println!("CPU Temp: {:.1} C", temp);
+                CPU_TEMP.get_or_create(&"zone0".to_owned()).set(temp);
+            }
+            Err(err) => {
+                eprintln!("Failed to read CPU temp: {:?}", err);
+            }
+        };
+    }
 }
