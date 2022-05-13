79bdae41aa5b9906202a5fd031201145d2f01ec3
Chathan Driehuys <chathan@driehuys.com>
2022-05-13 20:03:14 +0000

    feat: Output PWM signal.
    Example of outputting a PWM signal. This gives an example of what it
    will take to control a PWM fan.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, thread, time::Duration};
+
+use rppal::pwm::Pwm;
 
 const CPU_TEMP_FILE: &str = "/sys/class/thermal/thermal_zone0/temp";
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     match read_cpu_temp(CPU_TEMP_FILE) {
         Ok(temp) => {
             println!("CPU Temp: {:.1} C", temp);
-        },
+        }
         Err(err) => {
             eprintln!("Failed to read CPU temperature: {:?}", err);
         }
-    }
+    };
+
+    let pin = Pwm::with_frequency(
+        rppal::pwm::Channel::Pwm0,
+        50.0,
+        0.25,
+        rppal::pwm::Polarity::Normal,
+        true,
+    )?;
+
+    thread::sleep(Duration::from_secs(3));
+
+    pin.set_duty_cycle(0.5)?;
+    thread::sleep(Duration::from_secs(3));
+
+    pin.set_duty_cycle(0.75)?;
+    thread::sleep(Duration::from_secs(3));
+
+    pin.set_duty_cycle(1.0)?;
+    thread::sleep(Duration::from_secs(3));
+
+    Ok(())
 }
 
 fn read_cpu_temp(path: &str) -> Result<f32, Box<dyn Error>> {
